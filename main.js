7138289fa9bc49e2fe0e3d6bdd80b106a703351b
Apxika <konstantine.paloumbis@gmail.com>
2023-02-22 12:03:32 -0500

    Finished

--- main.js
+++ main.js
@@ -2,14 +2,70 @@
 // │ Your code here! │
 // └─────────────────┘
 
+function createUser (firstName, lastName) {
+    let user = {
+        firstName: firstName,
+        lastName: lastName
+    }
+    return user;
+}
 
+function setAge(user, num) {
+    user.age = num
+    return user
+}
 
+function incrementAge(user) {
+    user.age = user.age + 1
+    return user
+}
 
+function fixCar(car) {
+    car.needsMaintenance = false
+    return car
+}
 
+function addGrades(student, newGrades) {
+    for (let i = 0; i < newGrades.length; i++) {
+        student.grades.push(newGrades[i])
+    }
+    return student
+}
 
+function getDataType(car, x) {
+    return typeof car[x]
+}
 
+function addTodo(todos, newTodo) {
+    todos.push(newTodo)
+    return todos
+}
 
+function addSong(playlist, newSong) {
+    playlist.duration += newSong.duration
+    playlist.songs.push(newSong)
+    return playlist
+}
 
+function updateReportCard(reportCard, grade) {
+    if (grade > reportCard.highestGrade) {
+        reportCard.highestGrade = grade
+    } 
+    if (grade < reportCard.lowestGrade) {
+        reportCard.lowestGrade = grade
+    }
+    reportCard.grades.push(grade)
+
+    let sum = 0
+
+    for (let i = 0; i < reportCard.grades.length; i++) {
+        sum += reportCard.grades[i]
+    }
+    
+    reportCard.averageGrade = sum / reportCard.grades.length
+
+    return reportCard
+}
 
 // ┌─────────────────────────────────────────────────────────────────────────┐
 // │ Code used for testing. Do not modify!                                   │                         
